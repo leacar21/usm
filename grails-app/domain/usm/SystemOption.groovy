f409ea7567918ab314676bcf8753095903349782
Leandro Carrasco <carrascoleandro@MacBookLea.local>
2015-06-10 22:05:16 -0300

    * Avance

--- grails-app/domain/usm/SystemOption.groovy
+++ grails-app/domain/usm/SystemOption.groovy
@@ -3,11 +3,13 @@ package usm
 class SystemOption {
 
     String name
+	String description
     static belongsTo = [system: System]
     static hasMany = [accessTypes: AccessType]
 
     static constraints = {
-		name size: 3..50, blank: false
+		name size: 3..35, blank: false
+		description size: 3..75, blank: false
     }
 	
 	/*static mapping = {
